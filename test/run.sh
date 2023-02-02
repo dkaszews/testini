26dac8f5905622fdc0fe3a259cb31b3845fa76da
Dominik Kaszewski <dakaszewski@gmail.com>
2023-02-02 21:23:37 +0100

    Simplify test pattern

--- test/run.sh
+++ test/run.sh
@@ -3,44 +3,37 @@
 # While Testini is designed to need no runner scripts, it is better to write
 # a simple one for Testini itself. Otherwise, there is a risk that some fatal
 # error such as `return []` at top of `testini#run` might cause a failure that
-# is not detectable inside the framework itself.
+# is not detectable inside the framework itself. Plus, want to test failures.
 
 SCRIPT_ROOT=$(realpath $(dirname $0))
 TESTINI_ROOT=$(realpath $SCRIPT_ROOT/..)
 TESTINI_VIM=$(realpath $TESTINI_ROOT/plugin/testini.vim)
+EXPECTED=testini.?.expected.log
 
-diff_logs () {
-    actual=$1
-    expected="expected_${actual}"
-    diff -u $expected $actual
-    diff_failures=$(( $diff_failures + $? ))
+get_expected_exitcode() {
+    file=$(ls -1 $EXPECTED 2> /dev/null) && echo ${file//[^[0-9]/} || echo 0
 }
 
-test_failures=0
+total_failed=0
 pushd $SCRIPT_ROOT > /dev/null
 for dir in */; do
+    echo -e "Running tests from $(realpath $dir)"
     pushd $dir > /dev/null
-    echo "Running tests from $(realpath .)"
 
-    rm -f testini.log exitcode.log
     vim -u $TESTINI_VIM -c TestiniCi
-    echo $? > exitcode.log
-    diff_failures=0
-    diff_logs exitcode.log
-    diff_logs testini.log
-    if [[ $diff_failures -gt 0 ]]; then
-        test_failures=$(( $test_failures + 1 ))
-    fi
+    exitcode=$?
+    expected_exitcode=$(get_expected_exitcode)
+
+    failed=0
+    [[ $exitcode -ne $expected_exitcode ]] && failed=1 \
+        && echo "Expected exitcode: ${expected_exitcode}, actual: ${exitcode}"
+    diff -u --unidirectional-new-file $EXPECTED 'testini.log' || failed=1
+    (( total_failed += $failed ))
 
     popd > /dev/null
 done
 popd > /dev/null
 
-if [[ $test_failures -eq 0 ]]; then
-    echo 'All tests passed'
-    exit 0
-else
-    echo "${test_failures} tests failed!"
-    exit 1
-fi
+echo -e "\n${total_failed} tests failed"
+[[ $total_failed -eq 0 ]] && exit 0 || exit 1
 
